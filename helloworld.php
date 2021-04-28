68ba270ed890e6df424106dd5184ea2e70f31a4d
Gabriel Orgo <gabrielorgo5@gmail.com>
2021-04-28 02:32:59 -0300

    init hotfix

---
+++ helloworld.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- <meta http-equiv="refresh" content="3"> -->
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        echo "<p>Hello world</p>";
+        echo "<h2>Hello</h2>";
+        
+    ?>
+</body>
+</html>
